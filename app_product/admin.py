af1f974c19cf151f84c42da858d720b7f73bed9a
Ali Rahmani <alirahmani.py@gmail.com>
2021-06-27 10:45:28 +0430

    create category model

--- app_product/admin.py
+++ app_product/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from app_product.models import Product
+from app_product.models import Product, Category
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'name',
-        'pk'
+        'pk',
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'pk',
+        'is_sub',
     )
--- app_product/models.py
+++ app_product/models.py
@@ -1,10 +1,29 @@
-import os
 from datetime import datetime
 
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 
 from app_store.models import Store
+
+
+class Category(models.Model):
+    sub_category = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='subcategories',
+        null=True,
+        blank=True,
+        limit_choices_to={
+            'is_sub': False,
+        },
+    )
+    is_sub = models.BooleanField(default=False)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    register_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -14,6 +33,11 @@ class Product(models.Model):
         limit_choices_to={
             'is_confirm': True,
         },
+        related_name='products',
+    )
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
         related_name='products',
     )
     name = models.CharField(max_length=150)
